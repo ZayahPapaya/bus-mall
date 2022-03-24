88b4de7eeaf8c1de110f2943571dab349afe97da
Aiden <hyena.shenanigans@gmail.com>
2022-03-23 20:12:15 -0700

    Finalized chart functionality

--- js/app.js
+++ js/app.js
@@ -6,8 +6,11 @@ const resultsList = document.getElementById('results');
 const sessionRounds = 25;
 const varietyCount = 3;
 let currentRounds = 0;
-let advertList;
+let advertList = [];
 let lock = false;
+let chartLabels = [];
+let chartViews = [];
+let chartClicks = [];
 // varietyCount <= adArray.length is REQUIRED or else getRandom() goes infinite
 function Ad(name, imagePath) {
   this.name = name;
@@ -50,11 +53,14 @@ Ad.prototype.getRandom = function() {
   let selectionHold;
   for(let i = 0; i < varietyCount; i++) {
    selectionHold = Math.floor(Math.random() * adArray.length);
-    if(selectionCheck.includes(selectionHold)){
+    if(selectionCheck.includes(selectionHold) || advertList.includes(selectionHold)){
       i--;
     } else {
       selectionCheck.push(selectionHold);
     }
+  }
+  for(let j = 0; j < varietyCount; j++){
+    advertList.shift();
   }
   return selectionCheck;
 }
@@ -63,14 +69,22 @@ Ad.prototype.render = function() {
     gridContainer.removeChild(gridContainer.firstChild);
   };
   let imageSet = this.getRandom();
-  advertList = imageSet;
-  console.log(imageSet);
+  advertList = advertList.concat(imageSet);
+  console.log(advertList);
   for(let i = 0; i < imageSet.length; i++){
     let gridItem = document.createElement('img');
     gridItem.src = adArray[imageSet[i]].imagePath;
     gridItem.id = `${adArray[imageSet[i]].name}`
     gridContainer.appendChild(gridItem);
     adArray[imageSet[i]].viewCount++;
+  }
+}
+
+function defineData() {
+  for(let i = 0; i < adArray.length; i++){
+    chartLabels.push(adArray[i].name);
+    chartClicks.push(adArray[i].clickCount);
+    chartViews.push(adArray[i].viewCount);
   }
 }
  start.addEventListener('click', function (eventStart) {
@@ -102,6 +116,8 @@ Ad.prototype.render = function() {
    if(lock === false && currentRounds <= sessionRounds){
    currentRounds++;
    let clicky = eventAdClick.target.id
+   console.log(adArray);
+   console.log(advertList);
    for(let i = 0; i < advertList.length; i++){
      if(clicky === adArray[advertList[i]].name){
        for(let j = 0; j < adArray.length; j++){
@@ -116,32 +132,40 @@ Ad.prototype.render = function() {
   }
  });
  function chartRender(){
+    defineData();
     const ctx = document.getElementById('myChart').getContext('2d');
     const myChart = new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            labels: chartLabels,
             datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
+                label: '# of Views',
+                data: chartViews,
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                  'rgba(130, 80, 255, 0.2)',
+                  
                 ],
                 borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                  'rgba(130, 80, 255, 1)',
+                  
                 ],
+                
                 borderWidth: 1
-            }]
+            },
+            {
+              label: '# of Clicks',
+              data: chartClicks,
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+
+              ],
+              
+              borderWidth: 1
+          }]
         },
         options: {
             scales: {
